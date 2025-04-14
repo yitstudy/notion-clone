3cd23ebcf6afa676a1ce34aab5caefa5f42d29fa
yitstudy <yuito22suto@gmail.com>
2025-04-14 14:35:31 +0900

    Setting up initialization of supabase

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./Layout"
+import { Home } from "./pages/Home"
+import NoteDetail from "./pages/NoteDetail"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/notes/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
