468a0e377d4c1bf3bec90e9e44bf0b5095a3b18b
yitstudy <yuito22suto@gmail.com>
2025-04-17 20:39:24 +0900

    Note information is displayed on the detail screen

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -1,4 +1,4 @@
-import { TitleInput } from '@/components/Toolbar';
+import { TitleInput } from '@/components/TitleInput';
 import { useCurrentUserStore } from '@/modules/auth/current-user.state';
 import { noteRepository } from '@/modules/notes/note.repository';
 import { useNoteStore } from '@/modules/notes/note.state';
@@ -27,12 +27,11 @@ const NoteDetail = () => {
 
   if (isLoading) return <div/>
   if (note == null) return <div>note is not existed</div>;
-  console.log(note);
 
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
-        <TitleInput />s
+        <TitleInput initialData={note}/>
       </div>
     </div>
   );
