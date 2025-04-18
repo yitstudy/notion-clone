60e9dc2d6e07ee482a21581026804e7a2ef9de87
yitstudy <yuito22suto@gmail.com>
2025-04-18 19:39:18 +0900

    Allow users to navigate to the notes detail page from search results

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import SideBar from './components/SideBar';
 import { SearchModal } from './components/SearchModal';
 import { useCurrentUserStore } from './modules/auth/current-user.state';
@@ -8,6 +8,7 @@ import { noteRepository } from './modules/notes/note.repository';
 import { Note } from './modules/notes/note.entity';
 
 const Layout = () => {
+  const navigate = useNavigate();
   const { currentUser } = useCurrentUserStore();
   const noteStore = useNoteStore();
   const [isLoading, setIsLoading] = useState(false);
@@ -33,6 +34,11 @@ const Layout = () => {
     setSearchResult(notes);
   };
 
+  const moveToDetail = (noteId: number) => {
+    navigate(`/notes/${noteId}`);
+    setIsShowModal(false);
+  };
+
   if (currentUser == null) return <Navigate replace to="/signin" />;
 
   return (
@@ -43,7 +49,7 @@ const Layout = () => {
         <SearchModal
           isOpen={isShowModal}
           notes={searchResult}
-          onItemSelect={() => {}}
+          onItemSelect={moveToDetail}
           onKeywordChanged={searchNotes}
           onClose={() => setIsShowModal(false)}
         />
