8d78f339c9f046e49dffebcacea750face2d8f26
yitstudy <yuito22suto@gmail.com>
2025-04-18 19:06:34 +0900

    Calls the note retrieval process

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,12 +5,14 @@ import { useCurrentUserStore } from './modules/auth/current-user.state';
 import { useNoteStore } from './modules/notes/note.state';
 import { useEffect, useState } from 'react';
 import { noteRepository } from './modules/notes/note.repository';
+import { Note } from './modules/notes/note.entity';
 
 const Layout = () => {
   const { currentUser } = useCurrentUserStore();
   const noteStore = useNoteStore();
   const [isLoading, setIsLoading] = useState(false);
   const [isShowModal, setIsShowModal] = useState(false);
+  const [searchResult, setSearchResult] = useState<Note[]>([]);
 
   useEffect(() => {
     fetchNotes();
@@ -24,6 +26,13 @@ const Layout = () => {
     setIsLoading(false);
   };
 
+  const searchNotes = async (keyword: string) => {
+    const notes = await noteRepository.findByKeyboard(currentUser!.id, keyword);
+    if (notes == null) return;
+    noteStore.set(notes);
+    setSearchResult(notes);
+  };
+
   if (currentUser == null) return <Navigate replace to="/signin" />;
 
   return (
@@ -33,9 +42,9 @@ const Layout = () => {
         <Outlet />
         <SearchModal
           isOpen={isShowModal}
-          notes={[]}
+          notes={searchResult}
           onItemSelect={() => {}}
-          onKeywordChanged={() => {}}
+          onKeywordChanged={searchNotes}
           onClose={() => setIsShowModal(false)}
         />
       </main>
--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -7,6 +7,7 @@ import {
   CommandItem,
   CommandList,
 } from '@/components/ui/command';
+import { Note } from '@/modules/notes/note.entity';
 
 interface SearchModalProps {
   isOpen: boolean;
@@ -26,17 +27,17 @@ export function SearchModal({
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
       <Command shouldFilter={false}>
-        <CommandInput placeholder={'キーワードで検索'} />
+        <CommandInput placeholder={'キーワードで検索'} onValueChange={onKeywordChanged} />
         <CommandList>
           <CommandEmpty>条件に一致するノートがありません</CommandEmpty>
           <CommandGroup>
             {notes?.map((note) => (
               <CommandItem
                 key={note.id}
-                title={note.displayTitle}
+                title={note.title ?? '無題'}
                 onSelect={() => onItemSelect(note.id)}
               >
-                <span>{note.displayTitle}</span>
+                <span>{note.title ?? '無題'}</span>
               </CommandItem>
             ))}
           </CommandGroup>
