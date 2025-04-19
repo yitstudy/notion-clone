048fb734d37be4cf70611075296732d90eb51b23
yitstudy <yuito22suto@gmail.com>
2025-04-19 09:26:42 +0900

    Setting up real-time communication for supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,31 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, RealtimeChannel, RealtimePostgresChangesPayload } from "@supabase/supabase-js";
 import { Database } from "../../database.types.ts";
+import { Note } from "@/modules/notes/note.entity.ts";
 
 export const supabase = createClient<Database>(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_API_KEY
 );
+
+export const subscribe = (
+    userId: string,
+    callback:(payload: RealtimePostgresChangesPayload<Note>) => void
+) => {
+    return supabase
+    .channel('notes-changes')
+    .on<Note>(
+        'postgres_changes', 
+        { 
+            event: '*', 
+            schema: 'public', 
+            table: 'notes',
+            filter: `user_id=eq.${userId}`,
+        }, 
+        callback
+    )
+    .subscribe();
+};
+
+export const unsubscribe = (channel: RealtimeChannel) => {
+    supabase.removeChannel(channel);
+};
